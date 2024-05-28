d08c90133b88500c4b4325c284271152a771c508
pavlobarabah <pavlobarabah@gmail.com>
2024-05-28 23:18:05 +0300

    works with H2

--- src/main/java/com/example/spring_homework2/SpringHomework2Application.java
+++ src/main/java/com/example/spring_homework2/SpringHomework2Application.java
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SpringHomework2Application implements ApplicationRunner {
--- src/main/java/com/example/spring_homework2/service/EmployerService.java
+++ src/main/java/com/example/spring_homework2/service/EmployerService.java
@@ -1,5 +1,22 @@
 package com.example.spring_homework2.service;
 
-public interface EmployerService {
+import com.example.spring_homework2.domain.Employer;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+public interface EmployerService {
+    Employer save(Employer employer);
+
+    boolean delete(Employer employer);
+
+    void deleteAll();
+
+    void saveAll(Employer employer);
+
+    List<Employer> findAll();
+
+    boolean deleteById(long id);
+
+    Employer getOne(long id);
 }
